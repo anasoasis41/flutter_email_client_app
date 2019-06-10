8fc060f643f8168b39042395fee3f310ba9171a3
Anas Riahi <anas.riahi@smile-maroc.com>
2019-06-10 15:59:29 +0100

    Initial commit

---
+++ lib/Message.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
+
+
+part 'Message.g.dart';
+
+@JsonSerializable()
+class Message {
+  final String subject;
+  final String body;
+
+  Message(this.subject, this.body);
+
+  factory Message.fromJson(Map<String, dynamic> json) => _$MessageFromJson(json);
+
+  static Future<List<Message>> browse() async {
+      //String content = await rootBundle.loadString('data/messages.json');
+
+      http.Response response = await http.get("http://www.mocky.io/v2/5cfe50d9320000500045effb");
+
+      await Future.delayed(Duration(seconds: 3));
+
+      String content  = response.body;
+      // convert String to json
+      List collection = json.decode(content);
+      List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
+
+      return _messages;
+  }
+}
